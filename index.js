e9667264786970a750a8fff52ac47863c3b06181
Jia Yu Koh <kohjiayu1998@gmail.com>
2024-02-14 02:50:08 +0000

    connected to mongoDB and allow post

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
 const cors = require('cors'); 
-require('dotenv').config()
-const app = express();
-app.use(cors()); 
-app.use(express.json());
-const { engine } = require('express-handlebars');
+require('dotenv').config();
 const { connect } = require("./mongoUtil");
-const DB_NAME = process.env.DB_NAME;
-const statusRoutes = require("./routes/status")
+const statusRoutes = require("./routes/status");
 
+const app = express();
+
+// Middlewares
+app.use(cors()); 
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
+
+// Handlebars setup
+app.set("view engine", "hbs");
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// Adding handlebars helpers - if you actually use helpers in your templates, otherwise, this can be omitted.
+const helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+});
+
+// Connect to MongoDB
 async function main() {
-    const db = await connect(process.env.MONGO_URL, DB_NAME);
-    app.set("view engine", "hbs");
-    wax.on(hbs.handlebars);
-    wax.setLayoutPath("./views/layouts");
-    app.set("view engine", "hbs");
-    app.use(express.urlencoded({ extended: false }));
-    
-
-   
-    app.use('/status', statusRoutes)
-
-  
-
+    try {
+        await connect(process.env.MONGO_URL, process.env.DB_NAME);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Could not connect to MongoDB:", error);
+        process.exit(1); // Exit with error
+    }
+    //Routes
+    app.use('/status', statusRoutes);
 }
-
 
 main();
 
+
+
+// Start server
 app.listen(3000, function () {
-    console.log("Server has started");
-})
+    console.log("Server has started on port 3000");
+});
--- mongoUtil.js
+++ mongoUtil.js
@@ -2,11 +2,11 @@ const {MongoClient} = require('mongodb');
 let _db = null;
 
 
-async function connect(mongoURL, databaseName) {
-    const client = await MongoClient.connect(mongoURL);
+async function connect(MONGO_URL, DB_NAME) {
+    const client = await MongoClient.connect(MONGO_URL);
 
     // same as switching the database
-    const db = client.db(databaseName);
+    const db = client.db(DB_NAME);
     _db = db;
     return db;
 }
